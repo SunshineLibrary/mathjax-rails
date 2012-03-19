8bc6928b0b38a6bfe3e45bcd35ee9dae0faca7bc
Greg Woodward <gregwoodward0@gmail.com>
2012-03-19 13:37:42 -0700

    Upgraded to MathJax 2.0, 5612b3f296f44d56e433358164e0cdf281d42e16

--- lib/mathjax/rails/version.rb
+++ lib/mathjax/rails/version.rb
@@ -1,7 +1,7 @@
 module Mathjax
   module Rails
-    VERSION = '0.0.4'
-    MATHJAXVERSION = '1.1a'
-    DIRNAME = 'mathjax-MathJax-da8d488'
+    VERSION = '0.0.5'
+    MATHJAXVERSION = '2.0'
+    DIRNAME = 'mathjax-Mathjax-5612b3f'
   end
 end
